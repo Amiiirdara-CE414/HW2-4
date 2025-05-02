b9ef8f9f73bfce4b1ac9e2bdef1faceb7b7fa9af
AmirMahdi Daraei <79265218+amiiirdara@users.noreply.github.com>
2025-05-03 02:13:31 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "setup redirection..."
-iptables -t nat -A PREROUTING -p tcp --dport 0 -j REDIRECT --to-port 80
-echo "setup app..."
-python app.py
+sudo iptables -t nat -A PREROUTING -p tcp --dport 0 -j REDIRECT --to-port 80
+python3 app.py
